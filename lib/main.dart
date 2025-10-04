ab57c828181cd6e6fb8eb2f24f1c73cd207df04e
emiliyura <micaqaz@gmail.com>
2025-10-04 07:24:46 +0300

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -152,16 +152,20 @@ class SettingsScreen extends StatelessWidget {
             style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 12),
-          ElevatedButton(
-            onPressed: () {
-              showAboutDialog(
-                context: context,
-                applicationName: "Органайзер студента",
-                applicationVersion: "1.0",
-                children: const [Text("Практическая работа №3")],
-              );
-            },
-            child: const Text("О приложении"),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  showAboutDialog(
+                    context: context,
+                    applicationName: "Органайзер студента",
+                    applicationVersion: "1.0",
+                    children: const [Text("Практическая работа №3")],
+                  );
+                },
+                child: const Text("О приложении"),
+              ),
+            ],
           ),
         ],
       ),
